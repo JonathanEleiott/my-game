96acf00dd5d55bc88a2ebd244d957858e8efd0a6
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2022-12-15 10:32:26 -0500

    add initial state

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 // STATE
-
+const state = {
+  players: [],
+  score: 0,
+  guesses: [],
+  upgrades: [
+    {
+      name: 'small score upgrade',
+      cost: 25,
+      scorePerSecond: 1,
+      isHidden: true
+    },
+    {
+      name: 'medium score upgrade',
+      cost: 70,
+      scorePerSecond: 3,
+      isHidden: true
+    }
+  ],
+  health: 100
+}
 
 // HELPER FUNCTIONS
 
